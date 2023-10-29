2b1277d2052cca71da3c8848128a79987f772c12
JulioManoel <juliobonow@gmail.com>
2023-10-29 16:32:35 -0300

    config firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'fireabse/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATIwYgBLdjEfgpT3FQJ1dCjcDI5XyPyjY",
+  authDomain: "ati-upx6.firebaseapp.com",
+  projectId: "ati-upx6",
+  storageBucket: "ati-upx6.appspot.com",
+  messagingSenderId: "402106064816",
+  appId: "1:402106064816:web:bbbb9f0336d5443c910075"
+}
+
+const app = initializeApp(firebaseConfig)
+
+const auth = getAuth(app)
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+export { auth, db, storage }
